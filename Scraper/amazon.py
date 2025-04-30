92a2635ae7a6ca819b1c1edaee91dbb37c83a9bb
MIGUEL ANGEL <2008100185@UNTELS.EDU.PE>
2025-04-30 10:23:32 +0200

    Create amazon.py

---
+++ Scraper/amazon.py
@@ -0,0 +1,5 @@
+def obtener_ofertas_amazon():
+    return [
+        {"titulo": "DualSense Wireless Controller - Midnight Black", "precio": "59.99€", "url": "https://www.amazon.es/dualsense-black"},
+        {"titulo": "The Last of Us Part II Remastered", "precio": "39.90€", "url": "https://www.amazon.es/tlou2-remastered"}
+    ]
