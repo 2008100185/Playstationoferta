78b53b154f4ed3683bf0c2344aaa5f52daa9ae87
MIGUEL ANGEL <2008100185@UNTELS.EDU.PE>
2025-04-30 10:11:05 +0200

    Create eneba.py

---
+++ Scraper/eneba.py
@@ -0,0 +1,14 @@
+def obtener_ofertas_eneba():
+    # Simulación de scraper
+    return [
+        {
+            "titulo": "God of War Ragnarok - PS5",
+            "precio": "29.99€",
+            "url": "https://www.eneba.com/ps5-god-of-war-ragnarok"
+        },
+        {
+            "titulo": "PlayStation Plus 12 meses",
+            "precio": "44.99€",
+            "url": "https://www.eneba.com/playstation-plus-12-meses"
+        }
+    ]
