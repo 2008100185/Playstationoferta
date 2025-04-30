fca248f40f9f55f7440c97048b1158dc70747273
MIGUEL ANGEL <2008100185@UNTELS.EDU.PE>
2025-04-30 10:24:08 +0200

    Create Carrefour.py

---
+++ Scraper/Carrefour.py
@@ -0,0 +1,5 @@
+def obtener_ofertas_carrefour():
+    return [
+        {"titulo": "Ratchet & Clank PS5", "precio": "34.99€", "url": "https://www.carrefour.es/ratchet-ps5"},
+        {"titulo": "PSN Card 20€", "precio": "19.00€", "url": "https://www.carrefour.es/psn20"}
+    ]
